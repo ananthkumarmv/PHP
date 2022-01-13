f5aee9e7d3aec291655097e6198d2255ed810194
Ananth <mvananthkumar@gmail.com>
2022-01-13 11:03:00 +0530

    Create constant.php

---
+++ Learn-php/constant.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+
+
+<body>
+
+	<?php
+
+	define('PI', 3.142);
+	echo "The value of PI is: " . PI;
+
+	?>
+
+</body>
+</html>
