27d1514b1d823eaad63945f518d33338d193f73e
Ananth <mvananthkumar@gmail.com>
2022-01-13 11:03:06 +0530

    Create switch.php

---
+++ Learn-php/switch.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+
+
+<body>
+
+	<?php
+
+
+	$randNum = 5;
+
+	switch($randNum){
+		case 8:
+			echo "8";
+			break;
+
+		case 6:
+			echo "6";
+			break;
+
+		case 5:
+			echo "5";
+			break;
+
+		default:
+			echo "0";
+	}
+	
+	
+	?>
+
+</body>
+</html>
