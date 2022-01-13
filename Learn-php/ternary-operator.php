44823a4bc0f8040d613723ff16652d2a4acb86cf
Ananth <mvananthkumar@gmail.com>
2022-01-13 11:03:08 +0530

    Create ternary-operator.php

---
+++ Learn-php/ternary-operator.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title></title>
+</head>
+
+
+<body>
+
+	<?php
+
+
+	$randNum = 5;
+
+	// condition ? value if true : value if false
+
+	$biggestNum = (15 > 10) ? 15: 10;
+
+	echo $biggestNum;
+	
+	?>
+
+</body>
+</html>
